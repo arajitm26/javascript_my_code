4d55c32f8ed0d0b43f88bb55248803610d033c35
arajitm26 <127112358+arajitm26@users.noreply.github.com>
2024-01-31 13:15:09 +0530

    Create javascript_dynamic_object_key.js

---
+++ javascript_dynamic_object_key.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+</head>
+<body>
+    
+    <script type="text/javascript">
+      let key = 'name';
+      let obj = {key:"arajit"}
+      let objOne = {[key]:"arajit"}
+
+      console.log('obj',obj)
+      console.log('objOne',objOne)
+      
+    </script>
+</body>
+</html>
