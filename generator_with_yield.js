e311970bd9d798f4dbd60ba19884fe91b9c0300a
arajitm26 <127112358+arajitm26@users.noreply.github.com>
2024-04-22 16:15:51 +0530

    Create generator_with_yield.js

---
+++ generator_with_yield.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+</head>
+<body>
+
+<script type="text/javascript">
+  function* foo(index) {
+    while (index < 2) {
+      yield index;
+      index++;
+    }
+  }
+
+  const iterator = foo(0);
+
+  console.log(iterator.next());
+  // Expected output: 0
+
+  console.log(iterator.next());
+  // Expected output: 1
+</script>
+
+</body>
+</html>
